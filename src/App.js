e49ea580f8fd8f670bd67f24111eedefaef9f795
PDEX <isupd2002@yahoo.com.tw>
2018-03-09 15:54:22 +0800

    fic

--- src/App.js
+++ src/App.js
@@ -6,25 +6,46 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     person: [
-      { name: "John", age: 33 },
-      { name: "Bob", age: 15 },
-      { name: "Mary", age: 22 },
+      { id: 'ddd2', name: "John", age: 33 },
+      { id: 'fgrefr4', name: "Bob", age: 15 },
+      { id: 'ffrfr4', name: "Mary", age: 22 },
     ],
     count: 0,
     open: true,
   }
-  handleInputName = (inputName) => {
+  handleInputName = (event, id) => {
+    console.log(event, id);
+    //event為input事件的所有參數, id是所輸入的input的id(目前是亂數的key值)
+
+    const personIndex = this.state.person.findIndex(p => { return p.id === id });
+    console.log(personIndex);
+    //先用p.id去找出被改動的資料在陣列中是第幾筆
+    
+    const person = { ...this.state.person[personIndex] };
+    console.log(person);
+    //把該筆資料取出(避免值接修改state.person)
+    
+    person.name = event.target.value;
+    console.log(person.name);
+    //將該筆資料的name變成表單輸入的值
+    
+    const persons = [...this.state.person];
+    console.log(persons);
+    //令state.person為一個新陣列(避免值接修改state.person)
+    
+    persons[personIndex]=person;
+    console.log(persons[personIndex]);
+    //使新陣列中被改變的數值被改變
+    
     this.setState({
-      person: [
-        { name: "Joe", age: 33 },
-        { name: inputName.target.value, age: 15 },
-        { name: "Mary", age: 2 },
-      ]
+      person: persons,
     })
   }
-  handleDeleteName = (inputName) => {
-    const persons =this.state.person;
-    persons.splice(inputName,1);//將state中的person陣列從後面裁掉1個
+
+  handleDeleteName = (index) => {
+    //const persons =this.state.person;//將state指向新變數persons,但這個作法不好,因仍然指向原本的位置,最好使用ES6的解構重新做一個物件
+    const persons = [...this.state.person];
+    persons.splice(index, 1);//將state中的person陣列中選定與往後後裁掉1個
     this.setState({
       person: persons,
     })
@@ -62,8 +83,10 @@ class App extends Component {
             <Person
               name={man.name}
               age={man.age}
-              change={this.handleInputName}
-              delete={()=>this.handleDeleteName(index)}
+              key={man.id}
+              change={(event) => this.handleInputName(event, man.id)}
+              // event是輸入的值,man.id是陣列給予的id
+              delete={() => this.handleDeleteName(index)}
             />)
         })}</div>);
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,10 +3,11 @@ import './person.css';
 
 const person =(props)=>{
     return(
-        <div className='person'>
+        <div className='person' >
             <h2>I am a person</h2>
             <p onClick={props.click}>{`my name is ${props.name} and ${props.age} old`}</p>
-            <input type='text' onChange={props.change} value={props.name}/>      
+            <input type='text' onChange={props.change} value={props.name}/><br/>
+            <button onClick={props.delete}>delete</button>      
         </div>
     )
 };
